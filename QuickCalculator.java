a354157f2a64712e439f5683ac91e0d6bb467ef4
madelds <sibalmadel@gmail.com>
2023-09-17 21:05:26 -0500

    Comments for QuickCalculator

--- QuickCalculator.java
+++ QuickCalculator.java
@@ -1,18 +1,29 @@
+/**
+ * A simple command-line calculator program for evaluating mathematical expressions
+ * and storing variables using the BFCalculator.
+ */
 public class QuickCalculator {
     public static void main(String[] args) {
+        // Create a new instance of the BFCalculator for performing calculations
         BFCalculator calculator = new BFCalculator();
 
+        // Iterate through the command-line arguments provided
         for (String arg : args) {
             try {
+              // Check if the argument starts with "STORE"
                 if (arg.startsWith("STORE")) {
-                    calculator.store(arg);
+                    calculator.store(arg); // Store the variable using the BFCalculator
                 } else {
+                    // Evaluate the expression using the BFCalculator
                     BigFraction result = calculator.evaluate(arg);
+                    // Check if a valid result was obtained
                     if (result != null) {
+                        // Print the expression and its result
                         System.out.println(arg + " = " + result);
                     }
                 }
             } catch (Exception e) {
+                // Handle exceptions
                 e.printStackTrace();
             }
         }
